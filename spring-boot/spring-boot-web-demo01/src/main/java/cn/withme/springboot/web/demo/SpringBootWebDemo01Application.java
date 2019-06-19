942e52698025c5f96699ff0d5a5d3f2c3e37092d
liming <1234@qq.com>
2019-06-19 14:03:49 +0800

    增加actuator监控 ,调整spring-boot 模块管理

--- spring-boot/spring-boot-web-demo01/src/main/java/cn/withme/springboot/web/demo/SpringBootWebDemo01Application.java
+++ spring-boot/spring-boot-web-demo01/src/main/java/cn/withme/springboot/web/demo/SpringBootWebDemo01Application.java
@@ -2,12 +2,17 @@ package cn.withme.springboot.web.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class SpringBootWebDemo01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebDemo01Application.class, args);
+
+
     }
 
 }
