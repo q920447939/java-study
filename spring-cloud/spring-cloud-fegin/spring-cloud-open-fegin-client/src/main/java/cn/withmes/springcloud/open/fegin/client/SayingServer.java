942a372816bcbe639d5831e7b8bc797ddb04dfae
liming <1234@qq.com>
2019-06-21 17:17:03 +0800

    增加解释

--- spring-cloud/spring-cloud-fegin/spring-cloud-open-fegin-client/src/main/java/cn/withmes/springcloud/open/fegin/client/SayingServer.java
+++ spring-cloud/spring-cloud-fegin/spring-cloud-open-fegin-client/src/main/java/cn/withmes/springcloud/open/fegin/client/SayingServer.java
@@ -17,9 +17,12 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @Description:
  * @date 2019年06月21日
  */
+//需要和server定义的application.name 一样
 @FeignClient(value = "fegin-server")
 public interface SayingServer {
 
+    //此处其实就是走的http 协议
+    //栗子  /{server.name}:port/say/message?query=query
     @GetMapping("/say/message")
     String sayMessage(@RequestParam("message") String message);
 }
--- spring-cloud/spring-cloud-fegin/spring-cloud-open-fegin-server/src/main/java/cn/withmes/spring/cloud/open/fegin/server/FeginServer.java
+++ spring-cloud/spring-cloud-fegin/spring-cloud-open-fegin-server/src/main/java/cn/withmes/spring/cloud/open/fegin/server/FeginServer.java
@@ -10,12 +10,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication(scanBasePackages = "cn.withmes.spring.cloud.open.fegin.server")
-@EnableDiscoveryClient
+@EnableDiscoveryClient //注册到zk,默认本机 2181端口
 public class FeginServer {
 
     public static void main(String[] args) {
         Map<String, Object> params = new HashMap<>();
         params.put("server.port", 9001);
+        // client 会查找zk里面的节点叫做 fegin-server ,servier的apllication.name 要和 @FeignClient(value = "fegin-server")一样,
+        //否则在zk 找不到
         params.put("spring.application.name", "fegin-server");
         new SpringApplicationBuilder(FeginServer.class)
                 .properties(params)
