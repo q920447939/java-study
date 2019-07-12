7bbcc674cee75b17880aad5cd6969477c4216d57
liming <1234@qq.com>
2019-07-12 09:53:11 +0800

    rockmq 简单测试

---
+++ java-bases/src/main/java/cn/withme/rockmq/Consume.java
@@ -0,0 +1,54 @@
+/**
+ * @Project:
+ * @Author: leegoo
+ * @Date: 2019年07月11日
+ */
+package cn.withme.rockmq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * ClassName: Consume
+ * @Description:
+ * @author leegoo
+ * @date 2019年07月11日
+ */
+public class Consume {
+
+    public static void main(String[] args) throws InterruptedException, MQClientException {
+
+        // Instantiate with specified consumer group name.
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name");
+
+        // Specify name server addresses.
+        consumer.setNamesrvAddr("172.18.11.102:9876");
+
+        // Subscribe one more more topics to consume.
+        consumer.subscribe("TopicTest", "*");
+        // Register callback to execute on arrival of messages fetched from brokers.
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                MessageExt ext = msgs.get(0);
+                byte[] body = ext.getBody();
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), new String(body));
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        //Launch the consumer instance.
+        consumer.start();
+
+        System.out.printf("Consumer Started.%n");
+    }
+
+}
---
+++ java-bases/src/main/java/cn/withme/rockmq/RockerSimple.java
@@ -0,0 +1,43 @@
+/**
+ * @Project:
+ * @Author: leegoo
+ * @Date: 2019年07月11日
+ */
+package cn.withme.rockmq;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * ClassName: RockerSimple
+ * @Description:
+ * @author leegoo
+ * @date 2019年07月11日
+ */
+public class RockerSimple {
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new
+                DefaultMQProducer("please_rename_unique_group_name");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("172.18.11.102:9876");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 1; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("翔哥...." +
+                            i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+
+}
