5fbd4e9cb9bf8650cf9614c11c35c43d47959637
liming <1234@qq.com>
2019-07-12 09:50:07 +0800

    spring boot 优雅停止

--- spring-boot/spring-boot-web-demo01/src/main/java/cn/withme/springboot/web/demo/SpringBootWebDemo01Application.java
+++ spring-boot/spring-boot-web-demo01/src/main/java/cn/withme/springboot/web/demo/SpringBootWebDemo01Application.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class SpringBootWebDemo01Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootWebDemo01Application.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootWebDemo01Application.class, args);
+        applicationContext.stop();
 
 
     }
--- spring-boot/spring-boot-web-demo01/src/main/java/cn/withme/springboot/web/demo/controller/MyController.java
+++ spring-boot/spring-boot-web-demo01/src/main/java/cn/withme/springboot/web/demo/controller/MyController.java
@@ -12,11 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PreDestroy;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * ClassName: MyController
  *
  * @author leegoo
- * @Description:
+ * @Description: 测试spring boot优雅停止
  * @date 2019年05月22日
  */
 @RestController
@@ -25,10 +29,29 @@ public class MyController {
 
     private Student student;
 
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
     @GetMapping("/stu")
-    public Student getStudentMessage() {
-        System.out.println(student);
-        return student;
+    public String  getStudentMessage(int count) throws InterruptedException {
+        for (int i = 0; i < count; i++) {
+            executorService.execute(()->{
+                try {
+                    Thread.sleep(10000);
+                    System.out.println("正在睡眠....");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        return "完成";
+    }
+
+
+    @PreDestroy
+    public void Sss () {
+        executorService.shutdown();
+        System.out.println("关闭...");
     }
 
 }
