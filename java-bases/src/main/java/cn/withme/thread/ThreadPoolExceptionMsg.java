53e18af72f7921165d32f51c4ac4ee7e4408ad33
liming <1234@qq.com>
2019-07-15 10:24:16 +0800

    测试线程打印异常

---
+++ java-bases/src/main/java/cn/withme/thread/ThreadPoolExceptionMsg.java
@@ -0,0 +1,15 @@
+/**
+ * @Project:
+ * @Author: leegoo
+ * @Date: 2019年07月12日
+ */
+package cn.withme.thread;
+
+/**
+ * ClassName: ThreadPoolExceptionMes
+ * @Description:
+ * @author leegoo
+ * @date 2019年07月12日
+ */
+public class ThreadPoolExceptionMes {
+}
