455eb4ec42ac1bdc9c3ae7a0383d053d985a3be9
liming <liming@tangzhics.eicp.net>
2023-02-15 20:38:43 +0800

    深入理解Java虚拟机-对象的生命周期 深入理解Java虚拟机-类加载器

---
+++ jvm-exercise/jvm-exercise/src/main/java/myClassLoader/HotSwapClassLoader.java
@@ -0,0 +1,56 @@
+/**
+ * @Project:
+ * @Author: leegoo
+ * @Date: 2023年02月15日
+ */
+package myClassLoader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+// 定义一个类加载器
+public class HotSwapClassLoader extends ClassLoader {
+
+    // 加载类的路径
+    private String classPath;
+
+    public HotSwapClassLoader(String classPath) {
+        super(ClassLoader.getSystemClassLoader());
+        this.classPath = classPath;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] classData = getData(name);
+        if (classData == null) {
+            throw new ClassNotFoundException();
+        } else {
+            return defineClass(name, classData, 0, classData.length);
+        }
+    }
+
+    private byte[] getData(String className) {
+        // 从classPath中加载指定类的字节码文件
+        String path = classPath + File.separatorChar + className.replace('.', File.separatorChar) + ".class";
+        try {
+            InputStream ins = new FileInputStream(path);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int bufferSize = 4096;
+            byte[] buffer = new byte[bufferSize];
+            int bytesNumRead = 0;
+            // 读取类文件的字节码
+            while ((bytesNumRead = ins.read(buffer)) != -1) {
+                baos.write(buffer, 0, bytesNumRead);
+            }
+            return baos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
